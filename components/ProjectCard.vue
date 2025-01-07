dd9f1d3ba93bfbbd5cce78f30e2fb7717e32d341
eikyuu <v.duguet.dev@gmail.com>
2025-01-07 23:52:04 +0100

    reduce padding in ProjectCard component

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="bg-white z-0 relative shadow-lg p-20 items-center before:content-[''] before:absolute before:bg-[#FFC800] before:left-4 before:md:left-14 before:right-0 before:bottom-[-1rem] before:h-[1.5rem] before:w-11/12 before:transform before:scale-100 before:skew-x-[-15deg] before:-z-20 rounded-xl before:rounded">
+        class="bg-white z-0 relative shadow-lg p-10 items-center before:content-[''] before:absolute before:bg-[#FFC800] before:left-4 before:md:left-14 before:right-0 before:bottom-[-1rem] before:h-[1.5rem] before:w-11/12 before:transform before:scale-100 before:skew-x-[-15deg] before:-z-20 rounded-xl before:rounded">
 
         <div class="flex flex-col xl:flex-row">
             <NuxtImg :src="img" width="600" height="600" :class="`${order} rounded-md mb-10`" fit="cover" />
