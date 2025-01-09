136e582c56c37e6317679af2f2ef47d248d39736
eikyuu <v.duguet.dev@gmail.com>
2025-01-09 17:53:56 +0100

    feat: integrate Vercel Analytics component into app.vue and remove from index.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,12 @@
 <template>
+    <Analytics />
   <div>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt';
+</script>
