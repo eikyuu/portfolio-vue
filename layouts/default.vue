8caa335c069842b9ac16e648be9de59652fc1a1a
eikyuu <v.duguet.dev@gmail.com>
2025-03-18 10:53:50 +0100

    fix: increase maximum layout width for improved content display

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
 
   <Header />
 
-  <main class="w-[90vw] md:max-w-screen-xl mx-auto p-4 md:p-0 md:px-4 text-sm md:text-base leading-5 md:leading-8 mt-20 md:mt-32">
+  <main class="w-[90vw] xl:max-w-screen-2xl mx-auto p-4 md:p-0 md:px-4 text-sm md:text-base leading-5 md:leading-8 mt-20 md:mt-32">
     <slot />
   </main>   
   
