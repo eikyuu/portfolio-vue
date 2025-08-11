4dbc6079c53926bd80fdf57cc601b0415b7c3b53
eikyuu <v.duguet.dev@gmail.com>
2025-08-11 22:43:52 +0200

    height min cardgoldborder

--- components/CardGoldBorder.vue
+++ components/CardGoldBorder.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-white rounded-md shadow-xl shadow-[#F2EBF4] flex flex-col justify-between border-b-8 border-[#FFC800] p-5">
+    <div class="bg-white rounded-md shadow-xl shadow-[#F2EBF4] flex flex-col justify-between border-b-8 border-[#FFC800] p-5 min-h-[380px]">
         <slot />
     </div>
 </template>
